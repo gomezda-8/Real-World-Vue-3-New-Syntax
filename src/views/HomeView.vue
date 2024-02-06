5a2a983a5b97758acf2dacbfe478228c5504c36e
gomezda <david.gomezromero@swatchgroup.net>
2024-02-06 08:26:15 +0100

    L3-app

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,55 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import EventCard from '@/components/EventCard.vue';
+import { ref } from 'vue';
+
+const events = ref([
+  {
+    id: 5928101,
+    category: 'animal welfare',
+    title: 'Cat Adoption Day',
+    description: 'Find your new feline friend at this event.',
+    location: 'Meow Town',
+    date: 'January 28, 2024',
+    time: '12:00',
+    petsAllowed: 'true',
+    organizer: 'Kat Laydee',
+  },
+  {
+    id: 4582797,
+    category: 'food',
+    title: 'Community Gardering',
+    description: 'Join us as we tend to the community edible plants.',
+    location: 'Flora City',
+    date: 'March 14, 2024',
+    time: '10:00',
+    petsAllowed: true,
+    organizer: 'Fern Pollin',
+  },
+  {
+    id: 8419988,
+    category: 'sustainability',
+    title: 'Beach Cleanup',
+    description: 'Help pick up trash along the shore',
+    location: 'Playa del Carmen',
+    date: 'July 22, 2024',
+    time: '11:00',
+    petsAllowed: true,
+    organizer: 'Carey Wales',
+  }
+])
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h>Event For Good</h>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
 </template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
